02f09d795ff9f760f8adb3ae84b64cdc812a7308
badx86 <badlolpro@gmail.com>
2023-07-10 03:23:07 +0400

    add data folder and urls file

---
+++ data/urls.py
@@ -0,0 +1,14 @@
+MAIN_PAGE_URL = "https://demoqa.com/"
+
+# Elements
+TEXT_BOX_URL = "https://demoqa.com/text-box"
+CHECK_BOX_URL = "https://demoqa.com/checkbox"
+RADIO_BUTTON_URL = "https://demoqa.com/radio-button"
+WEB_TABLES_URL = "https://demoqa.com/webtables"
+BUTTONS_URL = "https://demoqa.com/buttons"
+LINKS_URL = "https://demoqa.com/links"
+BROKEN_LINKS_URL = "https://demoqa.com/broken"
+UPLOAD_AND_DOWNLOAD_URL = "https://demoqa.com/upload-download"
+DYNAMIC_PROPERTIES_URL = "https://demoqa.com/dynamic-properties"
+
+# Forms
