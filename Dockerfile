910bc86c56c0930128891606456476de1235540d
badx86 <badlolpro@gmail.com>
2023-07-17 20:16:36 +0400

    upd conftest

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,14 @@ COPY requirements.txt .
 # Установите зависимости проекта
 RUN pip install --no-cache-dir -r requirements.txt
 
+# Скачивание Chrome
+RUN wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Установка Chrome
+RUN dpkg -i google-chrome-stable_current_amd64.deb; apt-get -fy install
+
 # Скопируйте файлы вашего проекта в контейнер
 COPY . .
 
 # Запустите команду для запуска вашего приложения
-CMD ["pytest", "-s", "-v"]
+CMD [ "pytest", "-s", "-v", "--alluredir=allure-results" ]
--- conftest.py
+++ conftest.py
@@ -1,12 +1,25 @@
+import os
+
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.set_window_size(1920, 1080)
+    print('\nstart browser...')
+    chrome_options = Options()
+    if 'CI' in os.environ:
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver.set_window_size(1900, 1000)
+    else:
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+        driver.maximize_window()
     yield driver
+    print('\nquit browser...')
     driver.quit()
