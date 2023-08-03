5479b4c1f18440e0780cec971798eb6302f84902
Badx86 <Badx86@users.noreply.github.com>
2023-08-03 21:17:57 +0000

    Automated deployment: Thu Aug  3 21:17:57 UTC 2023 83c59509e83f0be241e06251d75f0f28e99e3e2d

--- data/elements.py
+++
@@ -1,9 +0,0 @@
-# Elements -> Check Box
-
-expected_elements = [
-    "Home", "Desktop", "Notes", "Commands", "Documents",
-    "WorkSpace", "React", "Angular", "Veu", "Office",
-    "Public", "Private", "Classified", "General", "Downloads",
-    "Word File.doc", "Excel File.doc"
-]
-
