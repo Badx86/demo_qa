821968095db2d5f9199d0882e5edc8c9f528ba8b
badx86 <badlolpro@gmail.com>
2023-08-26 01:41:07 +0400

    add generated_file for test_download_file

--- generator/generator.py
+++ generator/generator.py
@@ -12,10 +12,18 @@ def generated_person():
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
         firstname=faker_ru.first_name(),
         lastname=faker_ru.last_name(),
-        age=random.randint(18,55),
+        age=random.randint(18, 55),
         department=faker_ru.job(),
         salary=random.randint(10, 350000),
         email=faker_ru.email(),
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address(),
     )
+
+
+def generated_file():
+    path = rf"C:\Users\мвидео\PycharmProjects\DemoQA\filetest{random.randint(0,999)}.txt"
+    file = open(path, "w+")
+    file.write(f"Hello World!{random.randint(0,999)}")
+    file.close()
+    return file.name, path
